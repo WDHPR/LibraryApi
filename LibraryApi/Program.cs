c33f2d59214766452f675cf9228404fa00c5f9b0
Matt <widmer.matt@gmail.com>
2023-12-30 14:55:09 +0100

    WIP

--- LibraryApi/Program.cs
+++ LibraryApi/Program.cs
@@ -1,13 +1,37 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using LibraryApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Json serialization ignores looping references in Team/Players
+// Alternative to do later: solve with DTOs
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-builder.Services.AddControllers();
+// Set SqLite Database for development database
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Sqlite");
+    opt.UseSqlite(connectionString);
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//ensure empty DB for development
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
